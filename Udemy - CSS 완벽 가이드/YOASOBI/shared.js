bc0af46f9e261f4306295b1e4658f034d95ae086
thelight0804 <thelight0804@gmail.com>
2023-10-03 15:28:16 +0900

    Add hide modal when click no button
    no 버튼을 클릭하면 modal 숨기기

--- Udemy - CSS 완벽 가이드/YOASOBI/shared.js
+++ Udemy - CSS 완벽 가이드/YOASOBI/shared.js
@@ -1,3 +1,4 @@
+// display modal when click listen button
 var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector(".modal");
 var selectSongBtn = document.querySelectorAll(".song button");
@@ -8,3 +9,11 @@ for (var btn of selectSongBtn){
         modal.style.display = "block";
     })
 }
+
+// hide modal when click no button
+var selectNoBtn = document.querySelector(".modal__action--negative");
+
+selectNoBtn.addEventListener('click', ()=>{
+    backdrop.style.display = "none";
+    modal.style.display = "none";
+})
