a9816df4e94d98e9195d9201109cb4e04e946d1a
thelight0804 <thelight0804@gmail.com>
2023-10-03 16:55:06 +0900

    Refector add classList instead of change style
    자바스크립트에서 직접 스타일을 변경하지 않고, classList를 사용해서 css에 class를 추가하는 방식으로 토글 상호작용을 구현했습니다.

--- Udemy - CSS 완벽 가이드/YOASOBI/shared.js
+++ Udemy - CSS 완벽 가이드/YOASOBI/shared.js
@@ -5,8 +5,11 @@ var selectSongBtn = document.querySelectorAll(".song button");
 
 for (var btn of selectSongBtn){
     btn.addEventListener('click', () => {
-        backdrop.style.display = "block";
-        modal.style.display = "block";
+        // backdrop.style.display = "block";
+        // modal.style.display = "block";
+        // modal.className = "open";
+        backdrop.classList.add("open");
+        modal.classList.add("open");
     })
 }
 
@@ -14,8 +17,10 @@ for (var btn of selectSongBtn){
 var selectNoBtn = document.querySelector(".modal__action--negative");
 
 selectNoBtn.addEventListener('click', () => {
-    backdrop.style.display = "none";
-    modal.style.display = "none";
+    // backdrop.style.display = "none";
+    // modal.style.display = "none";
+    backdrop.classList.remove("open");
+    modal.classList.remove("open");
 })
 
 // open mobile toggle navigation
@@ -23,14 +28,19 @@ var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
 
 toggleButton.addEventListener('click', () => {
-    backdrop.style.display = "block";
-    mobileNav.style.display = "block";
+    // backdrop.style.display = "block";
+    // mobileNav.style.display = "block";
+    backdrop.classList.add("open");
+    mobileNav.classList.add("open");
 })
 
 // close mobile toggle navigation
 
 backdrop.addEventListener('click', () => {
-    backdrop.style.display = "none";
+    // backdrop.style.display = "none";
     mobileNav.style.display = "none";
-    modal.style.display = "none";
+    // modal.style.display = "none";
+    backdrop.classList.remove("open");
+    modal.classList.remove("open");
+    mobileNav.classList.remove("open");
 })
