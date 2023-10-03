4bc9c3d4f145f415563156382e15450262891433
thelight0804 <thelight0804@gmail.com>
2023-10-03 16:10:37 +0900

    Add open, close mobile nav
    - 햄버거 토글창 클릭 시 mobile nav 표시
    - backdrop 클릭 시 mobile nav와 modal 닫기

--- Udemy - CSS 완벽 가이드/YOASOBI/shared.js
+++ Udemy - CSS 완벽 가이드/YOASOBI/shared.js
@@ -4,7 +4,7 @@ var modal = document.querySelector(".modal");
 var selectSongBtn = document.querySelectorAll(".song button");
 
 for (var btn of selectSongBtn){
-    btn.addEventListener('click', ()=>{
+    btn.addEventListener('click', () => {
         backdrop.style.display = "block";
         modal.style.display = "block";
     })
@@ -13,7 +13,24 @@ for (var btn of selectSongBtn){
 // hide modal when click no button
 var selectNoBtn = document.querySelector(".modal__action--negative");
 
-selectNoBtn.addEventListener('click', ()=>{
+selectNoBtn.addEventListener('click', () => {
     backdrop.style.display = "none";
+    modal.style.display = "none";
+})
+
+// open mobile toggle navigation
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
+
+toggleButton.addEventListener('click', () => {
+    backdrop.style.display = "block";
+    mobileNav.style.display = "block";
+})
+
+// close mobile toggle navigation
+
+backdrop.addEventListener('click', () => {
+    backdrop.style.display = "none";
+    mobileNav.style.display = "none";
     modal.style.display = "none";
 })
